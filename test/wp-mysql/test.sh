f339285281f254c6e97f23f4b6d97bb9123be115
Midkard <dimigam@yandex.ru>
2023-08-04 22:24:18 +0300

    212

--- test/wp-mysql/test.sh
+++ test/wp-mysql/test.sh
@@ -3,7 +3,7 @@ cd $(dirname "$0")
 source test-utils.sh
 
 # Template specific tests
-check "wp" "$(ls -l /var/www/html/wp-config.php | grep SCRIPT_DEBUG)"
+check "wp" cat -l /var/www/html/wp-config.php | grep SCRIPT_DEBUG
 
 # Report result
 reportResults
